1f96f3e15a5c98f9f9223d44859ee0e8bf85778e
顾风 <88915462+Seuss1342@users.noreply.github.com>
2022-01-29 08:27:16 +0800

    Add files via upload

---
+++ bt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget http://download.bt.cn/install/update/LinuxPanel-7.7.0.zip
+unzip LinuxPanel-7.7.0.zip
+bash ./panel/update.sh
+rm -f /www/server/panel/data/bind.pl
+rm -f LinuxPanel-7.7.0.zip
+rm -rf ./panel
+sed -i "/#检查是否需要升级/{s/#检查是否需要升级/return public.returnMsg(False,updateInfo)/g}" /www/server/panel/class/ajax.py
+sed -i "/update6.sh/{s/update6/1/g}" /www/server/panel/task.py
+sed -i "/update6.sh/{s/update6/1/g}" /www/server/panel/tools.py
+sed -i "/retry_count/{s/3/300/g}" /www/server/panel/class/acme_v2.py
